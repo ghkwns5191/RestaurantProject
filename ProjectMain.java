e32d00cfb40d0547fbf7cda7f6e77818d029bafd
ghkwns5191 <ghkwns5191@gmail.com>
2022-06-13 00:27:18 +0900

    메인페이지 v.1

---
+++ ProjectMain.java
@@ -0,0 +1,18 @@
+package project;
+
+import java.awt.Choice;
+
+import project.Member.MemberDao;
+
+public class ProjectMain {
+
+	public static void main(String[] args) {
+		// 메인창
+		System.out.println("구동됨");
+		Login login = new Login();
+		
+		
+
+	}
+
+}
